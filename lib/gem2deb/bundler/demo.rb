dc3acb5068a3f5a470ec090a783c8d305c35e6d7
Lucas Nussbaum <lucas@debian.org>
2017-06-08 23:09:09 +0200

    Initialize new repository with 'bundler gem gem2deb-bundler-demo'

---
+++ lib/gem2deb/bundler/demo.rb
@@ -0,0 +1,9 @@
+require "gem2deb/bundler/demo/version"
+
+module Gem2deb
+  module Bundler
+    module Demo
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/gem2deb/bundler/demo/version.rb
@@ -0,0 +1,7 @@
+module Gem2deb
+  module Bundler
+    module Demo
+      VERSION = "0.1.0"
+    end
+  end
+end
